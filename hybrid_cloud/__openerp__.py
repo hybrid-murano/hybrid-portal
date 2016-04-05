cccde5855ccdccd389391f6e09d4e65951f9cea8
chenyujie <chenyujie@WIN-796UH84LRNK.cloud.com>
2016-04-05 21:56:36 +0800

    1. support user management

--- hybrid_cloud/__openerp__.py
+++ hybrid_cloud/__openerp__.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 {
     'name': 'Hybrid Cloud',
     'version': '1.0',
@@ -53,9 +32,6 @@ You can manage:
         'hybrid_view.xml',
         'hybrid_installer.xml',
         'hybrid_data.xml',
-#        'res_config_view.xml',
-#        'mail_hybrid_view.xml',
-        'res_users_view.xml',
         'views/hybrid.xml',
         'views/pages.xml',
     ],
@@ -70,4 +46,3 @@ You can manage:
     'auto_install': False,
     'qweb': [ 'static/src/xml/*.xml' ],
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- hybrid_cloud/hybrid.py
+++ hybrid_cloud/hybrid.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 import logging
 
 from openerp import SUPERUSER_ID
@@ -117,26 +96,17 @@ class hybrid_user(osv.Model):
 
     _columns = {
         #we need a related field in order to be able to sort the employee by name
-        'name_related': fields.related('resource_id', 'name', type='char', string='Name', readonly=True, store=True),
         'country_id': fields.many2one('res.country', 'Nationality'),
-        'birthday': fields.date("Date of Birth"),
-        'ssnid': fields.char('SSN No', help='Social Security Number'),
-        'sinid': fields.char('SIN No', help="Social Insurance Number"),
-        'identification_id': fields.char('Identification No'),
-        'otherid': fields.char('Other Id'),
         'gender': fields.selection([('male', 'Male'), ('female', 'Female')], 'Gender'),
-        'marital': fields.selection([('single', 'Single'), ('married', 'Married'), ('widower', 'Widower'), ('divorced', 'Divorced')], 'Marital Status'),
         'department_id': fields.many2one('hybrid.department', 'Department'),
         'address_id': fields.many2one('res.partner', 'Working Address'),
         'address_home_id': fields.many2one('res.partner', 'Home Address'),
-        'bank_account_id': fields.many2one('res.partner.bank', 'Bank Account Number', domain="[('partner_id','=',address_home_id)]", help="Employee bank salary account"),
         'work_phone': fields.char('Work Phone', readonly=False),
         'mobile_phone': fields.char('Work Mobile', readonly=False),
         'work_email': fields.char('Work Email', size=240),
         'work_location': fields.char('Office Location'),
         'notes': fields.text('Notes'),
         'category_ids': fields.many2many('hybrid.cloud', 'employee_category_rel', 'emp_id', 'category_id', 'Tags'),
-        'resource_id': fields.many2one('resource.resource', 'Resource', ondelete='cascade', required=True, auto_join=True),
         'coach_id': fields.many2one('hybrid.user', 'Coach'),
         # image: all image fields are base64 encoded and PIL-supported
         'image': fields.binary("Photo",
@@ -157,8 +127,6 @@ class hybrid_user(osv.Model):
             help="Small-sized photo of the employee. It is automatically "\
                  "resized as a 64x64px image, with aspect ratio preserved. "\
                  "Use this field anywhere a small image is required."),
-        'passport_id': fields.char('Passport No'),
-        'color': fields.integer('Color Index'),
         'city': fields.related('address_id', 'city', type='char', string='City'),
         'login': fields.related('user_id', 'login', type='char', string='Login', readonly=1),
         'last_login': fields.related('user_id', 'date', type='datetime', string='Latest Connection', readonly=1),
@@ -171,7 +139,6 @@ class hybrid_user(osv.Model):
     defaults = {
         'active': 1,
         'image': _get_default_image,
-        'color': 0,
     }
 
     def _broadcast_welcome(self, cr, uid, employee_id, context=None):
@@ -290,8 +257,8 @@ class hybrid_department(osv.Model):
         'name': fields.char('Department Name', required=True),
         'complete_name': fields.function(_dept_name_get_fnc, type="char", string='Name'),
         'company_id': fields.many2one('res.company', 'Company', select=True, required=False),
-        'manager_id': fields.many2one('hybrid.user', 'Manager'),
-        'member_ids': fields.one2many('hybrid.user', 'department_id', 'Members', readonly=True),
+        'manager_id': fields.many2one('res.users', 'Manager'),
+        'member_ids': fields.one2many('res.users', 'department_id', 'Members', readonly=True),
         'note': fields.text('Note'),
     }
 
@@ -317,9 +284,162 @@ class hybrid_department(osv.Model):
 class res_users(osv.osv):
     _name = 'res.users'
     _inherit = 'res.users'
+
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
+
     _columns = {
-        'employee_ids': fields.one2many('hybrid.user', 'user_id', 'Related employees'),
+        #we need a related field in order to be able to sort the employee by name
+        'country_id': fields.many2one('res.country', 'Nationality'),
+        'gender': fields.selection([('male', 'Male'), ('female', 'Female')], 'Gender'),
+        'department_id': fields.many2one('hybrid.department', 'Department'),
+        'address_id': fields.many2one('res.partner', 'Working Address'),
+        'address_home_id': fields.many2one('res.partner', 'Home Address'),
+        'work_phone': fields.char('Work Phone', readonly=False),
+        'mobile_phone': fields.char('Work Mobile', readonly=False),
+        'work_email': fields.char('Work Email', size=240),
+        'work_location': fields.char('Office Location'),
+        'notes': fields.text('Notes'),
+        'category_ids': fields.many2many('hybrid.cloud', 'employee_category_rel', 'emp_id', 'category_id', 'Tags'),
+        'coach_id': fields.many2one('res.users', 'Coach'),
+        # image: all image fields are base64 encoded and PIL-supported
+        'image': fields.binary("Photo",
+            help="This field holds the image used as photo for the employee, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'res.users': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Medium-sized photo of the employee. It is automatically "\
+                 "resized as a 128x128px image, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small': fields.function(_get_image, fnct_inv=_set_image,
+            string="Small-sized photo", type="binary", multi="_get_image",
+            store = {
+                'res.users': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Small-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image is required."),
+        'city': fields.related('address_id', 'city', type='char', string='City'),
+        'last_login': fields.related('user_id', 'date', type='datetime', string='Latest Connection', readonly=1),
     }
 
+    def _get_default_image(self, cr, uid, context=None):
+        image_path = get_module_resource('hybrid', 'static/src/img', 'default_image.png')
+        return tools.image_resize_image_big(open(image_path, 'rb').read().encode('base64'))
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    defaults = {
+        'active': 1,
+        'image': _get_default_image,
+    }
+
+    def _broadcast_welcome(self, cr, uid, employee_id, context=None):
+        """ Broadcast the welcome message to all users in the employee company. """
+        employee = self.browse(cr, uid, employee_id, context=context)
+        partner_ids = []
+        _model, group_id = self.pool['ir.model.data'].get_object_reference(cr, uid, 'base', 'group_user')
+        if employee.user_id:
+            company_id = employee.user_id.company_id.id
+        elif employee.company_id:
+            company_id = employee.company_id.id
+        elif employee.department_id:
+            company_id = employee.department_id.company_id.id
+        else:
+            company_id = self.pool['res.company']._company_default_get(cr, uid, 'res.users', context=context)
+        res_users = self.pool['res.users']
+        user_ids = res_users.search(
+            cr, SUPERUSER_ID, [
+                ('company_id', '=', company_id),
+                ('groups_id', 'in', group_id)
+            ], context=context)
+        partner_ids = list(set(u.partner_id.id for u in res_users.browse(cr, SUPERUSER_ID, user_ids, context=context)))
+        self.message_post(
+            cr, uid, [employee_id],
+            body=_('Welcome to %s! Please help him/her take the first steps with us!') % (employee.name),
+            partner_ids=partner_ids,
+            subtype='mail.mt_comment', context=context
+        )
+        return True
+
+    def create(self, cr, uid, data, context=None):
+        context = dict(context or {})
+        if context.get("mail_broadcast"):
+            context['mail_create_nolog'] = True
+
+        employee_id = super(res_users, self).create(cr, uid, data, context=context)
+
+        if context.get("mail_broadcast"):
+            self._broadcast_welcome(cr, uid, employee_id, context=context)
+        return employee_id
+
+    def unlink(self, cr, uid, ids, context=None):
+        resource_ids = []
+        for employee in self.browse(cr, uid, ids, context=context):
+            resource_ids.append(employee.resource_id.id)
+        super(res_users, self).unlink(cr, uid, ids, context=context)
+        return self.pool.get('resource.resource').unlink(cr, uid, resource_ids, context=context)
+
+    def onchange_address_id(self, cr, uid, ids, address, context=None):
+        if address:
+            address = self.pool.get('res.partner').browse(cr, uid, address, context=context)
+            return {'value': {'work_phone': address.phone, 'mobile_phone': address.mobile}}
+        return {'value': {}}
+
+    def onchange_company(self, cr, uid, ids, company, context=None):
+        address_id = False
+        if company:
+            company_id = self.pool.get('res.company').browse(cr, uid, company, context=context)
+            address = self.pool.get('res.partner').address_get(cr, uid, [company_id.partner_id.id], ['default'])
+            address_id = address and address['default'] or False
+        return {'value': {'address_id': address_id}}
+
+    def onchange_department_id(self, cr, uid, ids, department_id, context=None):
+        value = {'parent_id': False}
+        if department_id:
+            department = self.pool.get('hybrid.department').browse(cr, uid, department_id)
+            value['parent_id'] = department.manager_id.id
+        return {'value': value}
+
+    def onchange_user(self, cr, uid, ids, user_id, context=None):
+        work_email = False
+        if user_id:
+            work_email = self.pool.get('res.users').browse(cr, uid, user_id, context=context).email
+        return {'value': {'work_email': work_email}}
+
+    def action_follow(self, cr, uid, ids, context=None):
+        """ Wrapper because message_subscribe_users take a user_ids=None
+            that receive the context without the wrapper. """
+        return self.message_subscribe_users(cr, uid, ids, context=context)
+
+    def action_unfollow(self, cr, uid, ids, context=None):
+        """ Wrapper because message_unsubscribe_users take a user_ids=None
+            that receive the context without the wrapper. """
+        return self.message_unsubscribe_users(cr, uid, ids, context=context)
+
+    def get_suggested_thread(self, cr, uid, removed_suggested_threads=None, context=None):
+        """Show the suggestion of employees if display_employees_suggestions if the
+        user perference allows it. """
+        user = self.pool.get('res.users').browse(cr, uid, uid, context)
+        if not user.display_employees_suggestions:
+            return []
+        else:
+            return super(res_users, self).get_suggested_thread(cr, uid, removed_suggested_threads, context)
+
+    def _message_get_auto_subscribe_fields(self, cr, uid, updated_fields, auto_follow_fields=None, context=None):
+        """ Overwrite of the original method to always follow user_id field,
+        even when not track_visibility so that a user will follow it's employee
+        """
+        if auto_follow_fields is None:
+            auto_follow_fields = ['user_id']
+        user_field_lst = []
+        for name, field in self._fields.items():
+            if name in auto_follow_fields and name in updated_fields and field.comodel_name == 'res.users':
+                user_field_lst.append(name)
+        return user_field_lst
